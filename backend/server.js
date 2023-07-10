c7661ac9a32017e9bdd3d6fe25872eeeff3d2509
f464df32e9c8 <ivankhutso@gmail.com>
2023-07-10 15:07:15 +0200

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 -app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://mern-task-app.onrender.com"],
+    origin: ["http://localhost:9000"],
   })
 );
 
